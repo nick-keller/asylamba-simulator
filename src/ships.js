573ad373de475f95cb7f89698a8221a67cbf7dc1
Benjamin Mousseau <benjamin.mousseau@gmail.com>
2015-08-01 21:30:28 +0200

    Redid ship.js with all the infos

--- src/ships.js
+++ src/ships.js
@@ -1,50 +1,97 @@
 var ships = [
-    {
-        name: 'Pegase',
-        pev: 2
-    },
-    {
-        name: 'Satyre',
-        pev: 3
-    },
-    {
-        name: 'Chimere',
-        pev: 3
-    },
-    {
-        name: 'Sirene',
-        pev: 5
-    },
-    {
-        name: 'Dryade',
-        pev: 7
-    },
-    {
-        name: 'Meduse',
-        pev: 9
-    },
-    {
-        name: 'Griffon',
-        pev: 23
-    },
-    {
-        name: 'Cyclope',
-        pev: 45
-    },
-    {
-        name: 'Minotaure',
-        pev: 75
-    },
-    {
-        name: 'Hydre',
-        pev: 86
-    },
-    {
-        name: 'Cerbere',
-        pev: 82
-    },
-    {
-        name: 'Phenix',
-        pev: 84
-    }
+  {
+    name: 'Pegase',
+    defense: 1,
+    vitesse: 195,
+    coque: 26,
+    pev: 2,
+    attaques: [5]
+  },
+  {
+    name: 'Satyre',
+    defense: 5,
+    vitesse: 190,
+    coque: 33,
+    pev: 3,
+    attaques: [7]
+  },
+  {
+    nom: 'Chimere',
+    defense: 3,
+    vitesse: 195,
+    coque: 26,
+    pev: 3,
+    attaques: [6, 6]
+  {
+    name: 'Sirene',
+    defense: 6,
+    vitesse: 190,
+    coque: 65,
+    pev: 5,
+    attaques: [2, 2, 20]
+  },
+  {
+    name: 'Dryade',
+    defense: 23,
+    vitesse: 165,
+    coque: 90,
+    pev: 7,
+    attaques: [30]
+  },
+  {
+    name: 'Meduse',
+    defense: 8,
+    vitesse: 145,
+    coque: 75,
+    pev: 9,
+    attaques: [20, 12, 12, 12, 12, 7, 7, 7]
+  },
+  {
+    name: 'Griffon',
+    defense: 40,
+    vitesse: 100,
+    coque: 250,
+    pev: 23,
+    attaques: [15, 15, 15, 15]
+  },
+  {
+    name: 'Cyclope',
+    defense: 40,
+    vitesse: 90,
+    coque: 320,
+    pev: 45,
+    attaques: [225]
+  },
+  {
+    name: 'Minotaure',
+    defense: 120,
+    vitesse: 88,
+    coque: 1200,
+    pev: 75,
+    attaques: [10, 10, 25, 35, 35, 35, 35]
+  },
+  {
+    name: 'Hydre',
+    defense: 100,
+    vitesse: 75,
+    coque: 1050,
+    pev: 86,
+    attaques: [21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21] // lol
+  },
+  {
+    name: 'Cerbere',
+    defense: 135,
+    vitesse: 80,
+    coque: 1220,
+    pev: 82,
+    attaques: [6, 6, 6, 25, 25, 25, 25, 50, 50, 175]
+  },
+  {
+    name: 'Phenix',
+    defense: 150,
+    vitesse: 75,
+    coque: 1350,
+    pev: 84,
+    attaques: [6, 6, 6, 25, 25, 25, 25, 50, 50, 50, 50, 200]
+  }
 ];
